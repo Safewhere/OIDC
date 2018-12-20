324b2c4f43c08a7691623623e21b34f963772d77
letranxuanphuong <letranxuanphuong@gmail.com>
2018-12-20 11:27:56 +0700

    #62173 OIDC compliance - Support Hybrid OpenID Provider

---
+++ src/CSharp/AspnetWebMvc/AspnetWebMvc/OAuth2TokenResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspnetWebMvc
+{
+    public class OAuth2TokenResponse
+    {
+        public OAuth2TokenResponse() { }
+
+        public string AccessToken { get; set; }
+        public string IdToken { get; set; }
+        public string RefreshToken { get; set; }
+        public string TokenType { get; set; }
+        public int ExpiresIn { get; set; }
+    }
+}
